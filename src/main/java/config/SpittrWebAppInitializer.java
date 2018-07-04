39326b424ebc8799075e6f0e86d30fb5f8f8b7f7
tanjie1234 <zyzz521@gmail.com>
2018-07-04 17:12:44 +0800

    Initial commit

---
+++ src/main/java/config/SpittrWebAppInitializer.java
@@ -0,0 +1,24 @@
+package config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+/**
+ * @author tanjie
+ * @date 2018-07-04
+ */
+public class SpittrWebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{RootConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{WebConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -0,0 +1,21 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: tanjie
+  Date: 18-7-4
+  Time: 下午3:04
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Spittr</title>
+    <link rel="stylesheet"
+    type="text/css"
+    href="/WEB-INF/static/css/style.css">
+</head>
+<body>
+    <h1>Welcome to Spittr</h1>
+    <a href="/spittles">Spittles</a>
+    <a href="/spitter/register">Register</a>
+</body>
+</html>
